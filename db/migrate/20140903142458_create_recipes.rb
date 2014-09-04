7709ac9492e7d97413f43743c7ce96700230a7fb
Marcel Hoppe <hoppe.marcel@gmail.com>
2014-09-04 14:25:41 +0200

    new table "pictures" using in recipes list bit better look at recipies list

--- db/migrate/20140903142458_create_recipes.rb
+++ db/migrate/20140903142458_create_recipes.rb
@@ -3,9 +3,11 @@ class CreateRecipes < ActiveRecord::Migration
     create_table :recipes do |t|
       t.integer :prep_time
       t.string :name
-      t.string :instructions
+      t.text :instructions
       t.integer :food_value
       t.string :range
+      
+      t.integer :user_id
 
       t.timestamps
     end
