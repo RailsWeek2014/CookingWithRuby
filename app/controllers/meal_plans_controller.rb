864f9b332795c4ce9b3ed1dcdf4b2a06b6b86783
Dominic Althaus <althaus.dominic@gmail.com>
2014-09-06 13:07:00 +0200

    made date_picker a partial added a way to create and edit meal plans

--- app/controllers/meal_plans_controller.rb
+++ app/controllers/meal_plans_controller.rb
@@ -6,14 +6,39 @@ class MealPlansController < ApplicationController
   end
 
   def new
+    @meal_plan = MealPlan.new
   end
 
   def create
+    @meal_plan = MealPlan.new meal_plan_params
+    
+    if @meal_plan.date.future?
+      current_user.meal_plans << @meal_plan
+      redirect_to meal_plans_path
+    else
+      render action: 'new'
+    end
   end
 
   def destroy
   end
 
-  def update
+  def edit
+    @meal_plan = MealPlan.find(params[:id])
   end
+  
+  def update
+    @meal_plan = MealPlan.find(params[:id])
+          
+    if @meal_plan.update_attributes(meal_plan_params)
+      redirect_to meal_plans_path
+    else
+      render action: "edit"
+    end
+  end
+  
+  private
+    def meal_plan_params
+      params.require( 'meal_plan' ).permit(:recipe_id, :date, :daytime)
+    end
 end
