eee4ba676a3d1d884c89ea13900f904343d92794
Mareike Schilling <schilling.mareike@gmail.com>
2014-09-09 09:22:00 +0200

    new default-unit bugfix: replaced some occurrences short_name of units

--- db/migrate/20140920000000_insert_test_data.rb
+++ db/migrate/20140920000000_insert_test_data.rb
@@ -7,6 +7,7 @@ class InsertTestData < ActiveRecord::Migration
     
     Unit.create long_name: "Milliliter", name: "ml"
     Unit.create long_name: "Gramm", name: "g"
+    Unit.create long_name: "Stück", name: "St"
     
     Recipe.create name: "Heißes Zuckerwasser", prep_time: 15, 
       instructions: "Wasser zum Kochen bringen & Zucker einrühren",
@@ -15,9 +16,9 @@ class InsertTestData < ActiveRecord::Migration
     Quantity.create quantity: 1.0, unit_id: 1, ingredient_id: 1, recipe_id: 1
     Quantity.create quantity: 100.0, unit_id: 2, ingredient_id: 2, recipe_id: 1
     
-    Recipe.create name: "Registrated Test", prep_time: 1, instructions: "a", range: "registrated"
-    Recipe.create name: "Private Test", prep_time: 1, instructions: "b", range: "private"
-    Recipe.create name: "Public Test", prep_time: 1, instructions: "c", range: "public"
+    Recipe.create name: "Registrated Test", prep_time: 1, instructions: "a", range: "registrated", user_id: 1
+    Recipe.create name: "Private Test", prep_time: 1, instructions: "b", range: "private", user_id: 1
+    Recipe.create name: "Public Test", prep_time: 1, instructions: "c", range: "public", user_id: 1
 
     Category.create name: "Kuchen"
     Category.create name: "KeinKuchen"
