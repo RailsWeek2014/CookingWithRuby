8d04a1ea41be473707021cda35de27fa22ce8cc5
Marcel Hoppe <hoppe.marcel@gmail.com>
2014-09-09 11:00:57 +0200

    test with many_and_belongs_to

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,13 +3,13 @@ class Recipe < ActiveRecord::Base
   has_many :pictures
   has_many :quantities
   has_many :comments
-  has_many :recipe_categories
-  has_many :categories, through: :recipe_categories
   has_many :cookbook_entries
   has_many :cookbooks, through: :cookbook_entries
+
+  has_and_belongs_to_many :categories, autosave: true, inverse_of: :categoryies
   
   validates :name, presence: true
   validates :instructions, presence: true
   
-  accepts_nested_attributes_for :quantities, :recipe_categories, allow_destroy: true
+  accepts_nested_attributes_for :quantities, :categories, allow_destroy: true
 end
