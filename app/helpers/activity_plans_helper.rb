6dc0fd6e86aa5196b4765d6ba4258ef4f3c3d971
Mareike Schilling <schilling.mareike@gmail.com>
2014-09-11 13:54:27 +0200

    bugfix: basal metabolism

--- app/helpers/activity_plans_helper.rb
+++ app/helpers/activity_plans_helper.rb
@@ -13,11 +13,11 @@ module ActivityPlansHelper
   def getBasalMetabolism
     weight = current_user.weight
     height = current_user.height
-    age = Date.today.year - current_user.birth_date.year
     
     if(weight == nil || height == nil || current_user.birth_date == nil)
       return nil
     else
+      age = Date.today.year - current_user.birth_date.year
       if(current_user.sex == "m")
         return (14.24 * weight) + (64.06 * height) - (28.47 * age) - 4023.55
       else
