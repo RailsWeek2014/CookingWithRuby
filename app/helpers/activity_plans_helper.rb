a4e084a3475a42a342a9d13608cd9410d6530647
Mareike Schilling <schilling.mareike@gmail.com>
2014-09-11 11:34:20 +0200

    translation of devise messages (with devise-i18n gem)

--- app/helpers/activity_plans_helper.rb
+++ app/helpers/activity_plans_helper.rb
@@ -13,16 +13,15 @@ module ActivityPlansHelper
   def getBasalMetabolism
     weight = current_user.weight
     height = current_user.height
+    age = Date.today.year - current_user.birth_date.year
     
     if(weight == nil || height == nil || current_user.birth_date == nil)
       return nil
     else
-      age = Date.today.year - current_user.birth_date.year
-      
       if(current_user.sex == "m")
-        return 14.24 * weight + 64.06 * height - 28.47 * age - 4023.55
+        return (14.24 * weight) + (64.06 * height) - (28.47 * age) - 4023.55
       else
-        return 10.05 * weight + 37.68 * height - 19.68 * age - 272.14
+        return (10.05 * weight) + (37.68 * height) - (19.68 * age) - 272.14
       end
     end
   end
